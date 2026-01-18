888610ad53850a04efad7cfa195d32e40d7e469d
kurtulusn <nesat.kurtulus@commencis.com>
2026-01-19 02:08:50 +0300

    Develop: add PATextStyles

---
+++ lib/shared/widgets/pa_text_styles.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class PATextStyles {
+  static const TextStyle bold28 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+    color: Color(0xFF13231B),
+  );
+
+  static const TextStyle medium28 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w500,
+    color: Color(0xFF13231B),
+  );
+
+  static const TextStyle extraBold28 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w800,
+    color: Color(0xFF13231B),
+  );
+
+  static TextStyle regular16 = TextStyle(
+    fontSize: 16,
+    color: const Color(0xFF13231B).withOpacity(0.7),
+  );
+}
