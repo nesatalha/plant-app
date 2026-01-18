fc041abc30983334158f2276d304cee63614499e
kurtulusn <nesat.kurtulus@commencis.com>
2026-01-19 02:09:49 +0300

    Develop: add PAAssets class

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,6 @@
+class PAAssets {
+  static const String onboardingPage1Bg = 'assets/images/onboarding_page_1_bg.png';
+  static const String plant = 'assets/images/plant.png';
+  static const String phone = 'assets/images/phone.png';
+  static const String scan = 'assets/images/scan.png';
+}
