cd62903d7c1faabe63822daf7872a444344f1d6e
kurtulusn <nesat.kurtulus@commencis.com>
2026-01-19 03:07:28 +0300

    Develop: add dimensions class

---
+++ lib/core/constants/pa_dimens.dart
@@ -0,0 +1,7 @@
+abstract class PADimens {
+  static const double px8 = 8.0;
+  static const double px16 = 16.0;
+  static const double px20 = 20.0;
+  static const double px22 = 22.0;
+  static const double px24 = 24.0;
+}
