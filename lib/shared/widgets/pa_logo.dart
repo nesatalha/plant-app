09497940b82db3ee196404a780507d25e363a87b
kurtulusn <nesat.kurtulus@commencis.com>
2026-01-19 04:47:35 +0300

    Develop: add PlanApp logo widget

---
+++ lib/shared/widgets/pa_logo.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/core/constants/assets.dart';
+
+class PaLogo extends StatelessWidget {
+  double? height;
+
+  PaLogo({this.height, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      PAAssets.paLogo,
+      height: height,
+    );
+  }
+}
