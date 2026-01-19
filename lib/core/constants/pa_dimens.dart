3b53b3ae769fad1ba15fe5284ec1de957b38372d
kurtulusn <nesat.kurtulus@commencis.com>
2026-01-19 04:48:58 +0300

    Devleop: update assets & dimens

--- lib/core/constants/pa_dimens.dart
+++ lib/core/constants/pa_dimens.dart
@@ -1,7 +1,12 @@
 abstract class PADimens {
+  static const double px0 = 0.0;
   static const double px8 = 8.0;
+  static const double px12 = 12.0;
+  static const double px14 = 14.0;
   static const double px16 = 16.0;
   static const double px20 = 20.0;
   static const double px22 = 22.0;
   static const double px24 = 24.0;
+  static const double px36 = 36.0;
+  static const double px320 = 320.0;
 }
