cc8fc2ae432c88a2e9a9d98813f9d46bef248bff
kurtulusn <nesat.kurtulus@commencis.com>
2026-01-19 03:15:30 +0300

    Onboarding: add welcome page route to router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:plant_app/features/onboarding/presentation/pages/welcome_page.dart';
 
 part 'app_router.gr.dart';
 
@@ -6,6 +7,6 @@ part 'app_router.gr.dart';
 class AppRouter extends _$AppRouter {
   @override
   List<AutoRoute> get routes => [
-        // Routes will be added when pages are created
+        AutoRoute(page: WelcomeRoute.page, initial: true),
       ];
 }
