c319c733f472f84ff3114f84c1537f0a208b59f9
kurtulusn <nesat.kurtulus@commencis.com>
2026-01-19 03:40:15 +0300

    Develop: update app router and assets

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -1,6 +1,4 @@
 class PAAssets {
-  static const String onboardingPage1Bg = 'assets/images/onboarding_page_1_bg.png';
-  static const String plant = 'assets/images/plant.png';
-  static const String phone = 'assets/images/phone.png';
-  static const String scan = 'assets/images/scan.png';
+  static const String welcomePageBg = 'assets/images/welcome_page_bg.png';
+  static const String onboardingPage1Bg = 'assets/images/onboarding_1_bg.png';
 }
--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:plant_app/features/onboarding/presentation/pages/onboarding_page.dart';
 import 'package:plant_app/features/onboarding/presentation/pages/welcome_page.dart';
 
 part 'app_router.gr.dart';
@@ -8,5 +9,6 @@ class AppRouter extends _$AppRouter {
   @override
   List<AutoRoute> get routes => [
         AutoRoute(page: WelcomeRoute.page, initial: true),
+        AutoRoute(page: OnboardingRoute.page),
       ];
 }
