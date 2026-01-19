a72bb45ec96baafd69af822e8dd3e188bc25e533
kurtulusn <nesat.kurtulus@commencis.com>
2026-01-19 03:09:06 +0300

    Develop: update pa_colors

--- lib/shared/theme/pa_colors.dart
+++ lib/shared/theme/pa_colors.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 
 class PAColors {
   static const Color primary = Color(0xFF28AF6E);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color mainTextColor = Color(0xFF13231B);
+  static const Color grey = Color(0x13231BB2);
+  static const Color termsAndConditionsGrey = Color(0x597165B2);
 }
