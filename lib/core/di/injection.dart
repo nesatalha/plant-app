3880e23c16af5984bd7bce5ebf34922d07d713bf
kurtulusn <nesat.kurtulus@commencis.com>
2026-01-19 07:14:57 +0300

    Develop: update dependency injection

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -1,6 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:plant_app/core/network/network_manager.dart';
 import 'package:plant_app/core/utils/shared_preferences_service.dart';
+import 'package:plant_app/features/home/data/datasources/home_remote_data_source.dart';
+import 'package:plant_app/features/home/data/repositories/home_repository_impl.dart';
+import 'package:plant_app/features/home/domain/repositories/home_repository.dart';
+import 'package:plant_app/features/home/presentation/bloc/home_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final getIt = GetIt.instance;
@@ -11,5 +15,16 @@ Future<void> setupDependencyInjection() async {
   getIt.registerLazySingleton<NetworkManager>(() => NetworkManager());
   getIt.registerLazySingleton<SharedPreferencesService>(
     () => SharedPreferencesService(getIt<SharedPreferences>()),
+  );
+
+  // Home Feature
+  getIt.registerLazySingleton<HomeRemoteDataSource>(
+    () => HomeRemoteDataSourceImpl(networkManager: getIt<NetworkManager>()),
+  );
+  getIt.registerLazySingleton<HomeRepository>(
+    () => HomeRepositoryImpl(remoteDataSource: getIt<HomeRemoteDataSource>()),
+  );
+  getIt.registerFactory<HomeBloc>(
+    () => HomeBloc(homeRepository: getIt<HomeRepository>()),
   );
 }
